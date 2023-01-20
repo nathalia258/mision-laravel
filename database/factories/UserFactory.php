3583e52d77d58bbd64ac7711ee73becc06f828e1
AndresVargasA <afva2710@gmail.com>
2023-01-20 18:32:51 -0500

    Mision deportes xd

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+
+class UserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => Hash::make("qwerty123"),
+            'NIT' => $this->faker->unique()->numberBetween($min = 100000, $max = 900000),
+            'phone' => $this->faker->unique()->numberBetween($min = 1000000, $max = 9000000),
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function unverified()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'email_verified_at' => null,
+            ];
+        });
+    }
+}
